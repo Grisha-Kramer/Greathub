f5bbd7ae3aaf9fa46d1126a51107ecafdb3b3c7c
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-01-30 20:29:29 -0500

    SQL file committed

---
+++ GB.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS greatBay_DB
+
+CREATE DATABASE greatBay_DB
+
+USE greatBay_DB
+
+CREATE TABLE auctions(
+    id INT NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(100) NOT NULL,
+    category VARCHAR (40) NOT NULL,
+    starting_bid INT default 0,
+    highest_bid INT default 0,
+    PRIMARY KEY (id)
+);
