cdc0c139f62b864c13800e8dd597fe2049c78983
Ben F <56423163+bferriman@users.noreply.github.com>
2020-01-30 21:08:20 -0500

    implemented post item function

--- great-bay.js
+++ great-bay.js
@@ -1,6 +1,43 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+function postItem() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Item name:"
+    },
+    {
+      type: "input",
+      name: "category",
+      message: "Category:"
+    },
+    {
+      type: "input",
+      name: "startBid",
+      message: "Starting bid:"
+    }
+  ])
+  .then( value => {
+    let query = connection.query(
+      "INSERT INTO auctions SET ?",
+      {
+        item_name: value.name,
+        category: value.category,
+        starting_bid: value.startBid,
+        highest_bid: 0
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.log("New item successfully added");
+        start();
+      }
+    )
+  });
+}
+
+
 const connection = mysql.createConnection( {
   host: "localhost",
 
@@ -12,7 +49,7 @@ const connection = mysql.createConnection( {
 
   // Your password
   password: "h#191712sPs)7",
-  database: "ice_creamDB"
+  database: "greatbay_db"
 });
 
 connection.connect( err => {
@@ -37,10 +74,10 @@ function start() {
   ])
   .then( ({ choice }) => {
     if(choice === "Post an Item") {
-
+      postItem();
     }
     else if(choice === "Bid on an Item") {
-
+      start();
     }
     else {  //quit
       console.log("Goodbye!");
